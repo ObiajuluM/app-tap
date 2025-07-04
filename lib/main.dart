fbf1e68ba4f3babdd761dbe64685c02d574c16aa
ObiajuluM <oambanefo@gmail.com>
2025-07-04 10:31:02 +0100

    Add adaptive launcher icons and update main app theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(
+        title: 'App Tap',
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -44,11 +42,12 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text(
               'You have pushed the button this many times:',
             ),
